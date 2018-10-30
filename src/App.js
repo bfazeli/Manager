b2bf86282f1883a8ce114784bfbaccb4ebe3ae86
bfazeli <bfazeli1@gmail.com>
2018-10-29 22:22:51 -0700

    Update: Created an action and a reducer to update the email state.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
+import firebase from 'firebase'
 import reducers from './reducers'
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+    componentWillMount()  {
+        const config = {
+            apiKey: "AIzaSyB7NfJRGB4COR3_wpTtbEhQGn5-BkrHfjI",
+            authDomain: "manager-18270.firebaseapp.com",
+            databaseURL: "https://manager-18270.firebaseio.com",
+            projectId: "manager-18270",
+            storageBucket: "manager-18270.appspot.com",
+            messagingSenderId: "598074561687"
+        };
+        firebase.initializeApp(config);
+    }
+
     render() {
         return(
             <Provider store={createStore(reducers)}>
-                <View>
-                    <Text>Hello!</Text>
-                </View>
+                <LoginForm/>
             </Provider>
         )
     }
