fe3c290f9e9616dfe4022b44fdc7a4df765d78a6
bfazeli <bfazeli1@gmail.com>
2018-10-31 23:58:20 -0700

    Update: Started Setting up navigation.

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+class EmploueeList extends Component{
+    render() {
+        return(
+            <View>
+                <Text>EmploueeList</Text>
+                <Text>EmploueeList</Text>
+                <Text>EmploueeList</Text>
+                <Text>EmploueeList</Text>
+            </View>
+        )
+    }
+} 
